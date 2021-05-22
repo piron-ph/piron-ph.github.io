a8cf24bec2aedd977f8d3965da95af9e6206eb0d
P I R O N <74213014+piron-ph@users.noreply.github.com>
2021-05-22 19:55:36 +0800

    Major Update 1

---
+++ asset.js
@@ -0,0 +1,22 @@
+
+
+//Maintenance
+var maintenance = document.getElementById("maintenanceNotice");
+
+var btn = document.getElementById("BtnPnl");
+
+var span = document.getElementsByClassName("cL")[0];
+
+btn.onclick = function() {
+  maintenance.style.display = "block";
+}
+
+span.onclick = function() {
+  maintenance.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == maintenance) {
+    maintenance.style.display = "none";
+  }
+}
