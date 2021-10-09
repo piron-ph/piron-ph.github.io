46de2fcbbd98781c1cc7c70c711b69a031210c02
Ligolas Neo Malicdem <74213014+piron-ph@users.noreply.github.com>
2021-10-10 04:07:49 +0800

    About us

---
+++ countdown/asset.js
@@ -0,0 +1,26 @@
+//LIGOLAS NEO MALCIDEM
+var countDownDate = new Date("Oct 8, 2021 08:55:25").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+    
+  var distance = countDownDate - now;
+    
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+window.addEventListener("load", () => {
+ document.querySelector("body").classList.add("loaded"); 
+});
