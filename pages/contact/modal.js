d6a22a7e62f5538e9b614ef21e9d0bd1776819db
Ligolas Neo Malicdem <74213014+piron-ph@users.noreply.github.com>
2021-10-15 06:03:40 +0800

    Update

---
+++ pages/contact/modal.js
@@ -0,0 +1,8 @@
+function contactModal() {
+  var x = document.getElementById("Contact-deets");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
