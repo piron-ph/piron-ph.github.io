e90af19130e4d88afafbc9f3aafee3929428a43d
Ligolas Neo Malicdem <74213014+piron-ph@users.noreply.github.com>
2022-02-08 09:50:52 +0800

    YTSAB

---
+++ ytsab/Asset/Js/navbar/main.js
@@ -0,0 +1,22 @@
+const navMenu = document.getElementById('nav-menu'),
+      toggleMenu = document.getElementById('nav-toggle'),
+      closeMenu = document.getElementById('nav-close')
+
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
