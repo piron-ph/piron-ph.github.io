1f3194bafdb003976f81637b60276d192b4c35e8
Ligolas Neo Malicdem <74213014+piron-ph@users.noreply.github.com>
2022-02-09 11:26:52 +0800

    Update links1.0.0.js

--- ytsab/Asset/Js/navbar/links1.0.0.js
+++ ytsab/Asset/Js/navbar/links1.0.0.js
@@ -1,6 +1,6 @@
 function aboutUs() {
-  location.replace("piron-ph.github.io/")
+  location.replace("https://piron-ph.github.io/ytsab/Pages/About/About.html")
 }
 function shopEr() {
-  location.replace("piron-ph.github.io/")
+  location.replace("https://piron-ph.github.io/ytsab/Pages/Shop/Shop.html")
 }
