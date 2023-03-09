ad18a766ebb1e8fc624c626d1a4353100b60b9bb
Ligolas Neo Malicdem <74213014+piron-ph@users.noreply.github.com>
2023-03-10 03:52:21 +0800

    password

--- ash/assets/Scripts/js/pswk.js
+++ ash/assets/Scripts/js/pswk.js
@@ -2,7 +2,7 @@ function checkPswd() {
     var confirmPassword = "012124";
     var password = document.getElementById("019292914").value;
     if (password == confirmPassword) {
-         window.location="crs.html";
+         window.location="photos.html";
     }
     else{
         alert("Wrong password, babe, try again you can do it :)");
